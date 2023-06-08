b481b835874852711402acfd98b23a579872c7b9
Siviwe82 <130838536+Siviwe82@users.noreply.github.com>
2023-06-08 11:42:14 +0200

    Added JS and temperature

--- script.js
+++ script.js
@@ -17,18 +17,46 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt*1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x,png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
+ let apiKey = "b1b60e90e3e1e584041678da21127779";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+ axios.get(apiUrl).then(displayTemperature);
+    
+}
 
+function handleSybmit(event) {
+event.preventDefault();
+let cityElement = document.querySelector("#city-input");
+search(cityInputElement.value);
 
+}
 
-let apiKey = "b1b60e90e3e1e584041678da21127779";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cape Town&appid=${apiKey}&units=metric`
+function displayFahrenheitTemperature(event) {
+ event.preventDefault(); 
+ let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+ let temperatureElement = document.querySelector("#temperature")
+ temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSybmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+search("Cape Town")
